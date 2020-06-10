a430d8a7a27ed93f25a98c55fbb7ab9b8b140cfb
pguidi <50623805+pguidi@users.noreply.github.com>
2020-06-09 22:53:57 -0300

    Add files via upload

---
+++ source_code/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.discbtn = new System.Windows.Forms.Button();
+            this.Showdisc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.texttxtfile = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MILAN",
+            "E7(115)",
+            "E7(117)",
+            "E7(118)"});
+            this.comboBox1.Location = new System.Drawing.Point(59, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(230, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "OLT";
+            // 
+            // discbtn
+            // 
+            this.discbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discbtn.Location = new System.Drawing.Point(27, 61);
+            this.discbtn.Name = "discbtn";
+            this.discbtn.Size = new System.Drawing.Size(262, 25);
+            this.discbtn.TabIndex = 2;
+            this.discbtn.Text = "Discover ONT";
+            this.discbtn.UseVisualStyleBackColor = true;
+            this.discbtn.Click += new System.EventHandler(this.discbtn_Click_1);
+            // 
+            // Showdisc
+            // 
+            this.Showdisc.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.Showdisc.ForeColor = System.Drawing.SystemColors.Window;
+            this.Showdisc.Location = new System.Drawing.Point(27, 101);
+            this.Showdisc.Multiline = true;
+            this.Showdisc.Name = "Showdisc";
+            this.Showdisc.Size = new System.Drawing.Size(899, 239);
+            this.Showdisc.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(820, 361);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 35);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // texttxtfile
+            // 
+            this.texttxtfile.Location = new System.Drawing.Point(28, 361);
+            this.texttxtfile.Name = "texttxtfile";
+            this.texttxtfile.Size = new System.Drawing.Size(772, 232);
+            this.texttxtfile.TabIndex = 6;
+            this.texttxtfile.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(952, 618);
+            this.Controls.Add(this.texttxtfile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Showdisc);
+            this.Controls.Add(this.discbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button discbtn;
+        private System.Windows.Forms.TextBox Showdisc;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox texttxtfile;
+    }
+}
+
---
+++ source_code/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Renci.SshNet;
+using System.IO;
+
+namespace WindowsFormsApp2
+{
+
+    public partial class Form1 : Form
+    {
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Threading.ThreadStart threadStart = new System.Threading.ThreadStart(recvSSHData);
+            System.Threading.Thread thread = new System.Threading.Thread(threadStart);
+
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        private SshClient sshClient = null;
+        private ShellStream shellStreamSSH = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void discbtn_Click_1(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "MILAN")
+                 {
+
+                String sship = "";
+                //String sshuser = "";
+                String ipinicio = "IP*";
+                String ipfim = "*IP";
+                //String userinicio = "user*";
+                //String userfim = "*user";
+
+                StreamReader reader = File.OpenText("C:\\Test Cases\\DISCOVER_ONT\\DISCOVER_ONT_MILAN.txt");
+                string input = reader.ReadToEnd();
+                texttxtfile.Text = input;
+
+                int pos1ip = input.IndexOf(ipinicio);
+                int pos2ip = input.IndexOf(ipfim, pos1ip);
+                //int pos1user = input.IndexOf(userinicio);
+                //int pos2user = input.IndexOf(userfim, pos1user);
+
+                if (pos1ip >= 0)
+                {
+                    if(pos2ip >= 0)
+                    {
+                        sship = input.Substring(pos1ip, pos2ip - pos1ip);
+                    }
+                }
+
+                MessageBox.Show(sship);
+
+                //if (input.IndexOf(milanconfig) > -1)
+                //    MessageBox.Show(milanconfig);
+                //else
+                //    MessageBox.Show("não existe");         
+
+                //TextReader reader = new StreamReader(@"C:\Test Cases\DISCOVER_ONT\DISCOVER_ONT_MILAN.txt");
+                //texttxtfile.Text = reader.ReadToEnd();
+                //MessageBox.Show(reader);
+                //reader.Close();
+                try
+                    {
+
+                    this.sshClient = new SshClient(sship, "root", "root");
+                    this.sshClient.ConnectionInfo.Timeout = TimeSpan.FromSeconds(15);
+                    this.sshClient.Connect();
+                    this.shellStreamSSH = this.sshClient.CreateShellStream("DISCOVER ONT", 80, 60, 800, 600, 65536);
+                    System.Threading.Thread.Sleep(300);
+                    this.shellStreamSSH.Write("cli");
+                    this.shellStreamSSH.Write("\r\n");
+                    System.Threading.Thread.Sleep(300);
+                    this.shellStreamSSH.Write("show discovered-onts");
+                    this.shellStreamSSH.Write("\n");
+                    System.Threading.Thread.Sleep(500);
+                    this.sshClient.Disconnect();
+                    }
+                    catch (Exception exp)
+                    {
+                    MessageBox.Show("ERROR: " + exp.Message);
+                    }
+                  }              
+        }
+
+        private void appendTextBoxInThread(TextBox t, String s)
+        {
+            if(t.InvokeRequired)
+            {
+                t.Invoke(new Action<TextBox, string>(appendTextBoxInThread), new object[] { t, s });
+            }
+            else
+            {
+                t.AppendText(s);
+            }
+        }
+
+        private void recvSSHData()
+        {
+            while(true)
+            {
+                try
+                {
+                    if(this.shellStreamSSH !=null && this.shellStreamSSH.DataAvailable)
+                    {
+                        String strData = this.shellStreamSSH.Read();
+                        appendTextBoxInThread(Showdisc, strData);
+                    }
+                }
+                catch
+                {
+
+                }
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //if (sshClient.IsConnected)         
+             //   this.sshClient.Disconnect();        
+            //else
+             //   MessageBox.Show("Selecione uma OLT");        
+        }
+    }
+}
